e7f9edb13440daa1cb679adaa95a9e03d344cfe8
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-04-17 00:50:42 +0800

    [feat] Create user create endpoint

--- functions/src/modules/user.ts
+++ functions/src/modules/user.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
-import {db} from "../firebase";
+import {auth, db} from "../firebase";
+import {getCid} from "../utils";
 
 /**
  * Gets information about a user to be shown on user dashboard.
@@ -44,4 +45,79 @@ export const getInfo = functions
       prizeUnredeemed: prizeCountRef.data().count,
       transactions: transactionsRef.docs.map((doc) => doc.data()),
     };
+  });
+
+/**
+ * Create a new user with new unique conference id.
+ *
+ * @remarks
+ * This function is only callable when a user is authenticated, is type admin,
+ * and called using Cloud Function SDk.
+ *
+ * @param {Object} data - Consist of user's name, email and type.
+ * @param {string} data.name - Name of the user
+ * @param {string} data.email - Email of the user
+ * @param {string} data.type - Type of the user, one of 'participant','admin'
+ * ,'partner
+ *
+ * @returns {strin} cid - New conference id of user.
+ */
+export const create = functions
+  .region("asia-southeast1")
+  .https.onCall(async (data, context) => {
+    // Ensure user is authenticated
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        "unauthenticated",
+        "The function must be called while authenticated."
+      );
+    }
+    // Ensure function caller is an admin
+    const userRef = await db.users.doc(context.auth.uid).get();
+    if (!userRef.exists || userRef.data()?.type != "admin") {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "Not enough permissions to create user"
+      );
+    }
+    // Ensure data is well-formatted
+    if (
+      !data.name ||
+      !data.email ||
+      !["particpant", "partner", "admin"].includes(data.type)
+    ) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "Invalid arguments"
+      );
+    }
+
+    // Create user in firebase auth and update firestore document
+    try {
+      const cid = getCid();
+      await auth.createUser({
+        uid: cid,
+        email: data.email,
+        displayName: data.name,
+        emailVerified: true,
+        disabled: false,
+      });
+      await db.users.doc(cid).set({
+        name: data.name,
+        type: data.type,
+      });
+      return cid;
+    } catch (err: any) {
+      let detail;
+      functions.logger.error(err);
+      // Throw invaid argument error if error was due to firebase error
+      if (err.code === "auth/email-already-exists") {
+        detail = new Error("Email already added");
+      } else if (err.code == "auth/auth/uid-already-exists") {
+        detail = new Error("CID already exists, please try again");
+      } else {
+        detail = new Error("User is not added due to unknown error");
+      }
+      throw new functions.https.HttpsError("invalid-argument", detail.message);
+    }
   });
