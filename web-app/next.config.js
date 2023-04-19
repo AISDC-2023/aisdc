9faf9c2a6e50bb65d74c4542f07823a7266f8586
cweitat <cweitat@gmail.com>
2023-04-19 23:15:06 +0800

    update for deployment

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
