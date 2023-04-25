c21d6fd0b8d39f915187cd14dd2364184840e1ca
cweitat <chungweitat@fundigo.co>
2023-04-25 21:11:23 +0800

    [fix] disable react strict mode to disable cam

--- web-app/src/pages/login.jsx
+++ web-app/src/pages/login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { useRouter } from 'next/router'
 import { functions } from '@/firebase.js'
 import { httpsCallable } from 'firebase/functions'
@@ -8,74 +8,72 @@ import { Button } from '@/components/Button'
 import { Heading } from '@/components/Heading'
 import { QrReader } from 'react-qr-reader'
 import { InformationCircleIcon } from '@heroicons/react/24/outline'
-import { getAuth, sendSignInLinkToEmail } from 'firebase/auth'
 
 const QRScanner = (props) => {
+  const [isShown, setShowQr] = useState(true)
   const router = useRouter()
-  const scanCidRef = useRef(null);
+  const scanCidRef = useRef(null)
   return (
     <>
-      <QrReader
-        constraints={{ facingMode: 'environment' }}
-        scanDelay={1000}
-        onResult={async (result, error) => {
-          if (!!result) {
-            const cid = result?.text;
-            // Check if cid is 10 digit alphanumeric
-            if (cid && !cid.match(/^[a-zA-Z0-9]{10}$/)) {
-              // TODO: Find more elegant way to display this on the page
-              alert('Invalid QR Code')
-              return
-            }
-            // if cid is new then process
-            if (scanCidRef.current !== cid){
-              // valid cid so set
-              scanCidRef.current = cid;
-              const userVerify = httpsCallable(functions, 'user-verify')
-              userVerify({ cid: cid })
-                .then((result) => {
-                  //TODO : update code for path, test if FB function work first
-                  const type = result.data?.type;
-                  const email = result.data?.email;
-                  let path = ''
-                  switch (type) {
-                    case 'admin':
-                      path = '/admin?cid=' + cid
-                      break
-                    case 'partner':
-                      path = '/partner?cid=' + cid
-                      break
-                    case 'participant':
-                      path = '/participant?cid=' + cid
-                      break
-                    default:
-                      // If user's type is not defined, send them to register page
-                      // TODO: Need convert logic for this to .catch since now throwing 404 error
+      {isShown && (
+        <QrReader
+          style={{ width: '100%' }}
+          constraints={{ facingMode: 'environment' }}
+          scanDelay={1000}
+          onResult={async (result, error) => {
+            if (!!result) {
+              const cid = result?.text
+              // Check if cid is 10 digit alphanumeric
+              if (cid && !cid.match(/^[a-zA-Z0-9]{10}$/)) {
+                // stop the code from sending to server if invalid
+                return
+              }
+              // if cid is new then process
+              if (scanCidRef.current !== cid) {
+                // valid cid so set
+                scanCidRef.current = cid
+                const userVerify = httpsCallable(functions, 'user-verify')
+                userVerify({ cid: cid })
+                  .then((result) => {
+                    const type = result.data?.type
+                    const email = result.data?.email
+                    let params = ''
+                    if (
+                      type === 'admin' ||
+                      type === 'partner' ||
+                      type === 'participant'
+                    ) {
+                      params = '?cid=' + cid
+                    } else {
+                      setShowQr(false)
                       router.push('/register?cid=' + cid)
-                  }
+                    }
 
-                  sendEmail(cid, email).then(() => {
-                    // The link was successfully sent. Inform the user.
-                    window.localStorage.setItem('emailForSignIn', email)
-                    router.push('/email-sent');
-                  }).catch((error) => {
-                    console.error("Error occured while sending email");
-                    console.error(error);
-                  });
-                  // end
-              })
-              .catch((error) => {
-                console.log(error)
-              })
+                    setShowQr(false)
+                    sendEmail(params, email)
+                      .then(() => {
+                        // The link was successfully sent. Inform the user.
+                        window.localStorage.setItem('emailForSignIn', email)
+                        router.push('/email-sent')
+                      })
+                      .catch((error) => {
+                        console.error('Error occured while sending email')
+                        console.error(error)
+                      })
+                    // end
+                  })
+                  .catch((error) => {
+                    console.log(error)
+                  })
+              }
             }
-          }
-          if (!!error) {
-            // Commented error to prevent spamming the console
-            // console.info(error)
-          }
-        }}
-        style={{ width: '100%' }}
-      />
+            if (!!error) {
+              // Commented error to prevent spamming the console
+              // console.info(error)
+            }
+          }}
+        />
+      )}
     </>
   )
 }
