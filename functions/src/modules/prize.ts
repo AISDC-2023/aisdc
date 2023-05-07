028290babbe01757f8275b420ca01532c03fa531
xitij27 <79000102+xitij27@users.noreply.github.com>
2023-05-08 06:45:03 +0800

    [fix] admin check typo

--- functions/src/modules/prize.ts
+++ functions/src/modules/prize.ts
@@ -187,7 +187,7 @@ export const redeem = functions
       );
     }
     // Ensure function caller is an admin
-    if (!(context.auth.token?.type != "admin")) {
+    if (context.auth.token?.type != "admin") {
       throw new functions.https.HttpsError(
         "permission-denied",
         "Not enough permissions to create user"
