f8fac5fef7e29a4b219482a1090cc464e45ae0d9
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-05-07 17:00:11 +0800

    [feat] Add unregister workshop endpoint

--- functions/src/modules/workshop.ts
+++ functions/src/modules/workshop.ts
@@ -121,4 +121,68 @@ export const register = functions
     }
   });
 
-// export const unregister = null;
+/**
+ * Unregister user from a workshop
+ *
+ * @remarks
+ * This function is only callable when a user is authenticated and called using
+ * Cloud Function SDk.
+ *
+ * @param {Object} data - Consist of workshop id
+ * @param {string} data.wid - Id of the workshop
+ */
+export const unregister = functions
+  .region("asia-southeast1")
+  .https.onCall(async (data, context) => {
+    // // Ensure user is authenticated
+    // if (!context.auth) {
+    //   throw new functions.https.HttpsError(
+    //     "unauthenticated",
+    //     "The function must be called while authenticated."
+    //   );
+    // }
+    // const cid = context.auth.uid;
+    const cid = "rctNVZ48mQ";
+    const {wid} = data;
+    if (!wid) {
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "Workshop id is required"
+      );
+    }
+    try {
+      const workshopRef = await db.workshops.doc(wid).get();
+      const workshop = workshopRef.data();
+      if (!workshop) {
+        throw new functions.https.HttpsError(
+          "not-found",
+          `Workshop ${wid} not found`
+        );
+      }
+      if (!workshop.registeredUserIds.includes(cid)) {
+        throw new functions.https.HttpsError(
+          "already-exists",
+          "User not registered to workshop"
+        );
+      }
+      await workshopRef.ref.update({
+        registeredUserIds: workshop.registeredUserIds.filter((id) => id != cid),
+      });
+      await db.userTransactions(cid).add({
+        type: "workshop",
+        timestamp: FieldValue.serverTimestamp(),
+        description: `Unregistered from workshop ${workshop.name}`,
+      });
+    } catch (err) {
+      if (err instanceof functions.https.HttpsError) {
+        throw err;
+      }
+
+      functions.logger.error(err);
+      // Throw exception if unknown error
+      throw new functions.https.HttpsError(
+        "unknown",
+        "Unknown error occurred while unregistering from workshop."
+      );
+    }
+  });
