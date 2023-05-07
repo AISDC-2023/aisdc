e0f8ebb1acec92704be6c366f00dc17848dc76f5
xitij27 <79000102+xitij27@users.noreply.github.com>
2023-05-08 05:51:03 +0800

    added button to direct user from /admin to /participant

--- web-app/src/pages/admin/index.jsx
+++ web-app/src/pages/admin/index.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Usertable from './usertable'
 import Prizetable from './prizetable'
+import {Button} from './../../components/Button'
+import { Container } from 'react-bootstrap'
 
 const admin = () => {
   return (
     <>
+      <Container>
+      <Button href='/participant' style={{ textDecoration: 'none' }}> Participant's page </Button>
       <Usertable />
       <Prizetable />
+      </Container>
     </>
   )
 }
