bb218eb8fa375a8a3613c1461015d277740c9bcb
cweitat <chungweitat@fundigo.co>
2023-05-08 20:41:53 +0800

    fix #56 convert icons dots status to icons for prizes

--- web-app/src/components/participant/Prizes.jsx
+++ web-app/src/components/participant/Prizes.jsx
@@ -1,13 +1,36 @@
 import { Paragraph } from '@/components/Paragraph'
 import { Heading } from '@/components/Heading'
+import {
+  CircleStackIcon,
+  SquaresPlusIcon,
+} from '@heroicons/react/20/solid'
 
 const statuses = {
-  true: 'text-green-400 bg-green-400/10',
-  false: 'text-red-400 bg-red-400/10',
+  true: { icon: CircleStackIcon, bgColorClass: 'bg-green-600' },
+  false: { icon: SquaresPlusIcon, bgColorClass: 'bg-orange-600' }
 }
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
+}
+
+const EventTypeIcon = ({ qty }) => {
+    const type = qty > 0 ? 'true' : 'false'
+    const Icon = statuses[type].icon
+    return (
+      <>
+        <div>
+          <span
+            className={classNames(
+              statuses[type].bgColorClass,
+              'flex h-8 w-8 items-center justify-center rounded-full ring-8 ring-white'
+            )}
+          >
+            <Icon className="h-5 w-5 text-white" aria-hidden="true" />
+          </span>
+        </div>
+      </>
+    )
 }
 
 export function Prizes({ title, list }) {
@@ -19,26 +42,28 @@ export function Prizes({ title, list }) {
       {/* Activity Feed */}
       <div className="mt-6 flow-root">
         <ul role="list">
-          {list.map((item) => (
-            <li
-              key={item.name}
-              className="relative flex items-center space-x-3"
-            >
-              <div className="min-w-0 flex-auto">
-                <div className="flex items-center gap-x-3">
-                  <div
-                    className={classNames(
-                      statuses[item.quantity],
-                      'flex-none rounded-full p-1'
-                    )}
-                  >
-                    <div className="h-2 w-2 rounded-full bg-current" />
+          {list.length > 0 ? (
+              list.map((item, itemIdx) => (
+                <li key={itemIdx}>
+                  <div className="relative pb-8">
+                    <div className="relative flex space-x-3">
+                      {item ? (
+                        <EventTypeIcon qty={item.quantity}></EventTypeIcon>
+                      ) : null}
+                      <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                        <div>
+                          <Paragraph className="font-medium text-gray-900">
+                            {item.name}
+                          </Paragraph>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <Paragraph className="font-semibold">{item.name}</Paragraph>
-                </div>
-              </div>
-            </li>
-          ))}
+                </li>
+              ))
+            ) : (
+              <p className="mb-8 text-center">No prizes yet.</p>
+            )}
         </ul>
       </div>
     </div>
