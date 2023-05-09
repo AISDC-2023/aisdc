baa73d25cde993399af036f07d8d35772cdceaaa
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-05-09 21:51:01 +0800

    [fix] Update to first 50 rows

--- web-app/src/pages/admin/usertable.jsx
+++ web-app/src/pages/admin/usertable.jsx
@@ -160,7 +160,7 @@ const Usertable = () => {
                   </Button>
                 </td>
               </tr>
-            )).slice(0, 20)  // Show only first 20 result to avoid cluttering
+            )).slice(0, 50)  // Show only first 50 result to avoid cluttering
             }
         </tbody>
       </Table>
