49894c90c67d4432bffd539c2752007dc553d23a
xitij27 <79000102+xitij27@users.noreply.github.com>
2023-05-12 23:35:24 +0800

    [fix] Use stampCount for calculating lucky draw results

--- web-app/src/pages/admin/luckydraw.jsx
+++ web-app/src/pages/admin/luckydraw.jsx
@@ -8,22 +8,36 @@ import { httpsCallable } from 'firebase/functions'
 import { Transition } from '@headlessui/react'
 
 const userlistfunc = httpsCallable(functions, 'user-list')
+const userliststampfunc = httpsCallable(functions, 'user-listStamp')
 
 const Luckydraw = () => {
-  const [names, setNames] = useState([])
-  const [names2, setNames2] = useState([])
-  const [names3, setNames3] = useState([])
+  const [names, setNames] = useState([]) // The data of names is used for display
+  const [names2, setNames2] = useState([]) // This is used to save the names of those who have not won anything
+  const [names3, setNames3] = useState([]) // This has all the names. Used as a constant if we want to reset names
   const [initialLoad, setInitialLoad] = useState(false)
 
   useEffect(() => {
-    userlistfunc({})
+    // userlistfunc({})
+    //   .then((res) => {
+    //     const participants = res.data.users.filter(
+    //       (user) => user.type === 'participant'
+    //     )
+    //     setNames(participants)
+    //     setNames2(participants)
+    //     setNames3(participants)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+    userliststampfunc({})
       .then((res) => {
-        const participants = res.data.users.filter(
-          (user) => user.type === 'participant'
-        )
-        setNames(participants)
-        setNames2(participants)
-        setNames3(participants)
+        const namearr = res.data.map((participant) => {
+          const { name, stampCount } = participant;
+          return Array.from({ length: stampCount }, () => name);
+        }).flat();
+        setNames(namearr)
+        setNames2(namearr)
+        setNames3(namearr)
       })
       .catch((error) => {
         console.log(error)
@@ -42,19 +56,29 @@ const Luckydraw = () => {
   }, [initialLoad, names, startRaffle])
 
   function startRaffle() {
+    // if (names.length <= 1) {
+    //   return
+    // }
+    // const randomIndex = Math.floor(Math.random() * names.length)
+    // const filterOutNames = names.filter((name) => name !== names[randomIndex])
+    // setNames(filterOutNames)
+    // setInitialLoad(true)
+    
     if (names.length <= 1) {
-      return
+      return;
     }
-    const randomIndex = Math.floor(Math.random() * names.length)
-    const filterOutNames = names.filter((name) => name !== names[randomIndex])
-    setNames(filterOutNames)
-    setInitialLoad(true)
+  
+    const randomIndex = Math.floor(Math.random() * names.length);
+    const filteredNames = names.filter((_, index) => index !== randomIndex);
+  
+    setNames(filteredNames);
+    setInitialLoad(true);
   }
 
   function restartRaffle() {
     setInitialLoad(false);
     const remainingNames = names2.filter((name) => !names.includes(name))
-    if (remainingNames.length <= 1) { resetNames() }
+    if (remainingNames.length <= 1 || remainingNames.every((name) => name === remainingNames[0])) { resetNames() }
     else {
       setNames(remainingNames)
       setNames2(remainingNames)
@@ -95,12 +119,12 @@ const Luckydraw = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            {names.map((names, index) => (
+            {names.map((name, index) => (
               <span
                 key={index}
                 className="mr-3 mt-2 inline-flex items-center rounded-md bg-orange-100 px-2 py-1 text-sm font-medium text-yellow-800"
               >
-                {names.name}
+                {name}
               </span>
             ))}
           </Transition>
@@ -111,7 +135,7 @@ const Luckydraw = () => {
           enter="transition-opacity duration-500"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="transition-opacity duration-500"
+          leave="transition-opacity"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
@@ -121,7 +145,7 @@ const Luckydraw = () => {
           <div className="mt-5 flex justify-center">
             <Heading headerType="h1" className="text-orange-600">
               {' '}
-              {names[0]?.name}{' '}
+              {names[0]}{' '}
             </Heading>
           </div>
         </Transition>
